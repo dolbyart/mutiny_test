bb2a86dcfe325d79986ef58999f321eb65197b58
dolbyart <artfg@mail.ru>
2021-02-15 03:36:21 -0300

    fixes

--- src/main/java/model/Greeting.java
+++ src/main/java/model/Greeting.java
@@ -3,18 +3,20 @@ package model;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import io.smallrye.mutiny.Uni;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Greeting extends PanacheEntity {
 
     public String message;
 
-    public static Uni<String> greeting(String name){
+    public static Uni<String> greeting(String name) {
         return Uni.createFrom().item("Hello " + name);
     }
 
     public static Uni<Greeting> findRandom() {
-        Random random= new Random();
+        Random random = new Random();
 
        /* return Greeting.count()
                 .onItem().transform(x-> random.nextInt(x.intValue()))
@@ -23,9 +25,9 @@ public class Greeting extends PanacheEntity {
                 });*/
 
         return Greeting.count()
-                .map(x-> random.nextInt(x.intValue()))
-                .flatMap(index ->{
-                    return Greeting.findAll().page(index,1).firstResult();
+                .map(x -> random.nextInt(x.intValue()))
+                .flatMap(index -> {
+                    return Greeting.findAll().page(index, 1).firstResult();
                 });
     }
 }
